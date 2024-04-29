2b4c72b7b0e444d8e591d88f671f234969770e70
Olha <maol.zp.ua@gmail.com>
2024-04-30 01:33:54 +0300

    Added lecture-35

--- exercises/lecture-33/blog/README.md
+++ exercises/lecture-33/blog/README.md
@@ -1,3 +1,8 @@
+У папці lecture-33 знаходяться домашні завдання 33, 34, 35
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- exercises/lecture-33/blog/src/App.js
+++ exercises/lecture-33/blog/src/App.js
@@ -6,6 +6,12 @@ import Footer from './components/Footer';
 
 import Gallery from './components/Gallery';
 
+
+// lecture-35
+import Blog from './components/Blog';
+import Vitelogo from './Vitelogo.svg';
+
+
 function App() {
   // створити новий JSX-елемент з назвою jsxElement з таким вмістом:
   const jsxElement = <h1>I am a JSX element</h1>;
@@ -30,6 +36,17 @@ function App() {
 
   // створити новий JSX-елемент footer з таким вмістом:
   const footer = <p>Copyright &copy; 2024</p>;
+
+
+  //lecture-35
+  let post = {
+    id: 1,
+    title: "Programming Algorithm",
+    content: `The word Algorithm means “a process or set of rules to be followed in calculations or other problem-solving operations”. Therefore Algorithm refers to a set of rules/instructions that step-by-step define how a work is to be executed upon in order to get the expected results.`,
+    cover: "/Vitelogo.svg",
+    likes: 5
+  };
+
 
 
   return (
@@ -64,8 +81,32 @@ function App() {
       <Footer />
       </div>
 
+
+
+      {/* lecture-35 */}
+       <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={Vitelogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={logo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1> 
+      <Blog post={post} key={post.id} />
+    </>
+
+    
     </div>
   );
 }
 
+
+
+
+
 export default App;
+
+
+
